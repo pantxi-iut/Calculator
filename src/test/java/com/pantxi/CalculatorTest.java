d99b3605a8d4ca71973244f62488990c2530a794
Pantxika Dagorret <pantxika.dagorret@iutbayonne.univ-pau.fr>
2024-09-24 04:08:51 +0200

    méthodes de montage BeforeEach et démontage AfterEach

--- src/test/java/com/pantxi/CalculatorTest.java
+++ src/test/java/com/pantxi/CalculatorTest.java
@@ -1,15 +1,32 @@
 package com.pantxi;
 
+import org.junit.jupiter.api.*;
+/*
+L'instruction : import org.junit.jupiter.api.*     inclut (pour notre code) :
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+ */
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
+    
+    @BeforeEach
+    void setUp() {      // montage
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {       // démontage
+        calculator = null;
+    }
+
     @Test
     void add_devrait_calculer_la_somme_de_deux_int() {
         // GIVEN
-        Calculator calculator = new Calculator();
 
         // WHEN
         int somme = calculator.add(1,2);
@@ -22,7 +39,6 @@ class CalculatorTest {
     void divide_devrait_retourner_quotient_nul_quand_diviseur_plus_grand_que_dividende()
     {
         // GIVEN
-        Calculator calculator = new Calculator();
 
         // WHEN
         int quotient = calculator.divide(1,2);
